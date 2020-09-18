7310490a3bdeb849e5a46083333aba56f512248f
Masonrapa <33574271+masonrapa@users.noreply.github.com>
2020-09-18 19:09:21 +0200

    Create 3.java

---
+++ java/Useless/3.java
@@ -0,0 +1,9 @@
+//Useless code just for make GitHub view that java took me the most of my time : )
+//Ignore this file <3
+//By The way, my social networks:
+
+public class HelloWorld {
+  public static void Main(String[] args) {
+    System.out.println("www.bit.ly/masonrapa");
+  }
+}
